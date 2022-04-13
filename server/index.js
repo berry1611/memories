310632fdd53d3cdf46b8b0d2e47b5ce7ded682a2
berry1611 <berryshen16@gmail.com>
2022-04-13 15:43:22 +0700

    Add Frontend and Backend, get and post feature to /posts

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = process.env.DATABASE_ACCESS;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
