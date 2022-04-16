a5411f0df172567e79d5dce438fa7ea0fb4fabe0
berry1611 <berryshen16@gmail.com>
2022-04-16 20:45:55 +0700

    fix fetch url to backend

--- server/index.js
+++ server/index.js
@@ -4,15 +4,19 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-import 'dotenv/config';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+  res.send('Welcome to Memories API');
+});
 
 const CONNECTION_URL = process.env.DATABASE_ACCESS;
 const PORT = process.env.PORT || 5000;
@@ -20,4 +24,4 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(error));
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -11,7 +11,7 @@ const postSchema = mongoose.Schema({
     default: 0,
   },
   createdAt: {
-    type: Date,
+    type: Date.now(),
     default: new Date(),
   },
 });
